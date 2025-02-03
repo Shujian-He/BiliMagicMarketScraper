4cea357dc3ee8a96a5cfa512dec6a5a907bc834c
Shujian <hesj@outlook.jp>
2025-02-03 18:09:33 +0800

    major update, wrapped function

--- main.py
+++ main.py
@@ -44,124 +44,129 @@ parser.add_argument(
 
 args = parser.parse_args()
 wantList = args.want
-priceFilters = args.price
-discountFilters = args.discount
+priceFilter = args.price
+discountFilter = args.discount
 
 print("Want List:", wantList)
-print("Price Filters:", priceFilters)
+print("Price Filters:", priceFilter)
 
-# define URL for market
-url = "https://mall.bilibili.com/mall-magic-c/internet/c2c/v2/list"
+def run(wantList, priceFilter, discountFilter):
+    # define URL for market
+    url = "https://mall.bilibili.com/mall-magic-c/internet/c2c/v2/list"
 
-# defime filenames
-timeForFilename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-wantFile = f"want_{timeForFilename}.csv"
-totalFile = f"total_{timeForFilename}.csv"
+    # defime filenames
+    timeForFilename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    wantFile = f"want_{timeForFilename}.csv"
+    totalFile = f"total_{timeForFilename}.csv"
 
-# define nextId
-nextId = None
+    # define nextId
+    nextId = None
 
-# define start time for the loop
-startTime = int(time.time())
-print("startTime:", startTime)
+    # define start time for the loop
+    startTime = time.time()
+    print("Start Time:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(startTime)))
 
-while True:
-    payload = json.dumps({
-        "categoryFilter": "2312", # categoryFilter: 2312 for figure
-        "priceFilters": priceFilters,
-        "discountFilters": discountFilters,
-        "nextId": nextId
-    })
+    while True:
+        payload = json.dumps({
+            "categoryFilter": "2312", # categoryFilter: 2312 for figure
+            "priceFilters": priceFilter,
+            "discountFilters": discountFilter,
+            "nextId": nextId
+        })
 
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_1 like Mac OS X) AppleWebKit/619.2.8.10.7 (KHTML, like Gecko) Mobile/22B83 BiliApp/81900100 os/ios model/iPhone 14 Pro mobi_app/iphone build/81900100 osVer/18.1 network/2 channel/AppStore',
-        'Cookie': load_cookie()
-    }
-    try:
-        # sleep for 60s after 10min running
-        currTime = int(time.time())
-        print("currTime:", currTime)
-        if currTime - startTime > 600:
-            print("Start to sleep for 60s")
-            for i in range(0,60):
-                time.sleep(1)
-                print(f"{i+1}s passed...")
-            print("Slept for 60s")
-            startTime = int(time.time())
-        
-        # send request
-        response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
-        # print(response.text)
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_1 like Mac OS X) AppleWebKit/619.2.8.10.7 (KHTML, like Gecko) Mobile/22B83 BiliApp/81900100 os/ios model/iPhone 14 Pro mobi_app/iphone build/81900100 osVer/18.1 network/2 channel/AppStore',
+            'Cookie': load_cookie()
+        }
         try:
-            responseData = response.json()
-        except Exception as e:
-            print("Error Decoding json:", e)
-            continue
-
-        while responseData["code"] != 0:
-            print("\nError occurred when getting data. Retrying...")
+            # sleep for 60s after 10min running
+            currTime = time.time()
+            print("currTime:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(currTime)))
+            if currTime - startTime > 600:
+                print("Start to sleep for 60s")
+                for i in range(0,60):
+                    time.sleep(1)
+                    print(f"{i+1}s passed...")
+                print("Slept for 60s")
+                startTime = time.time()
+            
+            # send request
             response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
-            # print(response.text)
             try:
                 responseData = response.json()
             except Exception as e:
+                print(response.text)
                 print("Error Decoding json:", e)
                 continue
 
-        # update nextId
-        nextId = responseData["data"]["nextId"]
-        if nextId is None:
-            print("\nEnd reached.")
-            break
-        
-        # extract data & process
-        data = responseData["data"]["data"]
-        for item in data:
-            timeNow = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-            item['time'] = timeNow
+            while responseData["code"] != 0:
+                print(responseData)
+                print("\nError occurred when getting data. Retrying...")
+                response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
+                try:
+                    responseData = response.json()
+                except Exception as e:
+                    print(response.text)
+                    print("Error Decoding json:", e)
+                    continue
 
-            # skip package with multiple items
-            if item['totalItemsCount'] > 1:
-                continue
-
-            name = item["c2cItemsName"].replace("\n", " ").replace(",", " ").strip() # to avoid linefeed & comma
-            id = item['c2cItemsId']
-            price = item['price']
-            marketPrice = item['detailDtoList'][0]['marketPrice']
-            rate = int(price) / int(marketPrice)
-
-            with open(totalFile, "a") as file:
-                file.write(f"{timeNow},{name},{id},{price},{marketPrice},{rate}\n")
-
-            for wantName in wantList:
-                if wantName in name:
-                    with open(wantFile, "a") as file:
-                        file.write(f"{timeNow},{name},{id},{price},{marketPrice},{rate}\n")
-
-        # 90% probability to sleep for short time
-        if random.random() < 0.9:
-            time.sleep(random.uniform(1, 2))
-        # 10% probability to sleep for lone time
-        else:
-            time.sleep(random.uniform(2, 3))
+            # update nextId
+            nextId = responseData["data"]["nextId"]
+            if nextId is None:
+                print("\nEnd reached.")
+                break
             
-    except requests.exceptions.Timeout:
-        print("\nThe request timed out")
-        continue
-        
-    except KeyboardInterrupt:
-        print("\nProcess interrupted by user.")
-        break
+            # extract data & process
+            data = responseData["data"]["data"]
+            for item in data:
+                timeNow = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                item['time'] = timeNow
 
-    except Exception as e:
-        print("\nUnknown error occurred.")
-        print("Error:", e)
-        break
+                # skip package with multiple items
+                if item['totalItemsCount'] > 1:
+                    continue
 
-conn = initialize_database()
+                name = item["c2cItemsName"].replace("\n", " ").replace(",", " ").strip() # to avoid linefeed & comma
+                id = item['c2cItemsId']
+                price = item['price']
+                marketPrice = item['detailDtoList'][0]['marketPrice']
+                rate = int(price) / int(marketPrice)
 
-if os.path.exists(totalFile):
-    read_and_save(totalFile, conn)
-else:
-    print(f"File '{totalFile}' does not exist.")
+                with open(totalFile, "a") as file:
+                    file.write(f"{timeNow},{name},{id},{price},{marketPrice},{rate}\n")
+
+                for wantName in wantList:
+                    if wantName in name:
+                        with open(wantFile, "a") as file:
+                            file.write(f"{timeNow},{name},{id},{price},{marketPrice},{rate}\n")
+
+            # 90% probability to sleep for short time
+            if random.random() < 0.9:
+                time.sleep(random.uniform(1, 2))
+            # 10% probability to sleep for lone time
+            else:
+                time.sleep(random.uniform(2, 3))
+                
+        except requests.exceptions.Timeout:
+            print("\nThe request timed out")
+            continue
+            
+        except KeyboardInterrupt:
+            print("\nProcess interrupted by user.")
+            break
+
+        except Exception as e:
+            print("\nUnknown error occurred.")
+            print("Error:", e)
+            break
+
+    conn = initialize_database()
+
+    if os.path.exists(totalFile):
+        read_and_save(totalFile, conn)
+    else:
+        print(f"File '{totalFile}' does not exist.")
+
+if __name__ == "__main__":
+    run(wantList, priceFilter, discountFilter)
